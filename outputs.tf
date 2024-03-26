1e19a98a8e023408f626380b165fb7ba4dab5bc0
RolfMoleman <RolfMoleman@users.noreply.github.com>
2024-03-26 01:17:18 +0000

    [MegaLinter] Apply linters fixes

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "unique-seed" {
   description = "Custom value for the random characters to be used"
-  value = coalesce(var.unique-seed, local.random_safe_generation)
+  value       = coalesce(var.unique-seed, local.random_safe_generation)
 }
 
 output "validation" {
   description = "Validtion rules applied"
-  value = local.validation
+  value       = local.validation
 }
 output "organization" {
   value       = local.azdo.organization
