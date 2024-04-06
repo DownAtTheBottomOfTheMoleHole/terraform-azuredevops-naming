cf2877e5d974142853b4a75309b39113b2d0f1d2
Carl Dawson <106377191+RolfMoleman@users.noreply.github.com>
2024-04-06 09:14:24 +0100

    Update example2.tf

--- examples/example2.tf
+++ examples/example2.tf
@@ -8,8 +8,8 @@
   "uat",
   "prd"
 ]
-         prefix  = []
-         suffix  = []
+         prefix  = ["dbmh"]
+         suffix  = ["test"]
          unique_include_numbers  = true
          unique_length  = 4
          unique_seed  = ""
@@ -17,7 +17,7 @@
   }
 
 resource "azuredevops_project" "example" {
-  name               = "Example Project"
+  name               = module.azdo_naming.project.name
   visibility         = "private"
   version_control    = "Git"
   work_item_template = "Agile"
@@ -25,15 +25,15 @@ resource "azuredevops_project" "example" {
 
 resource "azuredevops_git_repository" "example" {
   project_id = azuredevops_project.example.id
-  name       = "Example Git Repository"
+  name       = module.azdo_naming.git_repository.name
   initialization {
     init_type = "Clean"
   }
 }
 
-resource "azuredevops_git_repository_branch" "example" {
+resource "azuredevops_git_repository_branch" "feature_branch" {
   repository_id = azuredevops_git_repository.example.id
-  name          = "example-branch-name"
+  name          = module.azdo_naming.git_repository_feature_branch_slash.name_unique
   ref_branch    = azuredevops_git_repository.example.default_branch
 }
 
