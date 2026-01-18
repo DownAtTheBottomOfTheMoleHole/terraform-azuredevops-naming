fe0245d77401f11d2b511b769f18d22421e1e09d
Carl Dawson <106377191+RolfMoleman@users.noreply.github.com>
2026-01-18 18:57:37 +0000

    Update required Terraform and provider versions in versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.10.0, <2.0.0"
+  required_version = ">= 1.14.0, < 2.0.0"
   required_providers {
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.7.0, <4.0.0"
+      version = ">= 3.8.0, < 4.0.0"
     }
   }
 }
